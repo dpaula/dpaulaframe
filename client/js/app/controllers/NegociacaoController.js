12ebfc592dbf34f94f8eb1dd00d4d05a66f2cf5c
dpaula <fernando.dpaula@gmail.com>
2018-03-12 22:30:32 -0300

    bind para document

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -2,6 +2,23 @@ class NegociacaoController{
 
     adiciona(event){
         event.preventDefault();
-        alert('Ação no controller');
+
+        let $ = document.querySelector.bind(document);
+        let inputData = $('#data');
+        let inputQuantidade = $('#quantidade');
+        let inputValor = $('#valor');
+        
+        console.log(inputData);
+        console.log(inputQuantidade);
+        console.log(inputValor);
+        
+    }
+    
+    carregaIniciais(){
+        let $ = document.querySelector.bind(document);
+        $('#data').value = '2015-01-01';
+        $('#quantidade').value = '10';
+        $('#valor').value = '2';
+        
     }
 }
