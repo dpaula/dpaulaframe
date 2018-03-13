0a099314308bace112a6903e1b6acb1eb3a3f411
dpaula <fernando.dpaula@gmail.com>
2018-03-12 22:47:16 -0300

    dom na listagem como propriedades

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -1,16 +1,19 @@
 class NegociacaoController{
 
+    constructor(){
+        let $ = document.querySelector.bind(document);
+        this.inputData = $('#data');
+        this.inputQuantidade = $('#quantidade');
+        this.inputValor = $('#valor');
+
+    }
+
     adiciona(event){
         event.preventDefault();
-
-        let $ = document.querySelector.bind(document);
-        let inputData = $('#data');
-        let inputQuantidade = $('#quantidade');
-        let inputValor = $('#valor');
         
-        console.log(inputData);
-        console.log(inputQuantidade);
-        console.log(inputValor);
+        console.log(this.inputData.value);
+        console.log(this.inputQuantidade.value);
+        console.log(this.inputValor.value);
         
     }
     
