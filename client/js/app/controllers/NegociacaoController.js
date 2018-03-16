0efb13f4509882909f01722291fad0e95e4d610b
dpaula <fernando.dpaula@gmail.com>
2018-03-15 22:51:22 -0300

    incluido um conversor para datas

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -11,29 +11,18 @@ class NegociacaoController{
     adiciona(event){
         event.preventDefault();
         
-        const d2 = new Date(this._inputData.value.split('-'));
-        console.log(d2);
-        
-        let data = new Date(
-            ...this._inputData.value
-            .split('-')
-            .map((item, indice) => item - indice % 2)
-        );
+        const dtHelper = new DataHelper();
+        let data = dtHelper.textoParaData(this._inputData.value);
 
-        console.log(data);
+        console.log('texto para data: '+data);
+        console.log('data para texto: '+dtHelper.dataParaTexto(data));
         
 
         let negociacao = new Negociacao(
-            this._inputData.value,
+            data,
             this._inputQuantidade.value,
             this._inputValor.value
         );
-
-        
-        
-        
-        console.log(typeof(this._inputData.value));
-        
     }
     
     carregaIniciais(){
@@ -41,11 +30,6 @@ class NegociacaoController{
         $('#data').value = '2015-01-01';
         $('#quantidade').value = '10';
         $('#valor').value = '2';
-        
-        let d1 = new Date([2011, 10, 20]);
-        console.log(d1);
-        let d3 = new Date('2011,10,25');
-        console.log(d3);
         
     }
 }
