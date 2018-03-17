ef7d9889f80c6525f454bb154d8506c01d73b411
dpaula <fernando.dpaula@gmail.com>
2018-03-17 19:18:02 -0300

    Utilizado template string e fail fast na validação da data

--- client/js/app/shared/DataHelper.js
+++ client/js/app/shared/DataHelper.js
@@ -1,16 +1,25 @@
 class DataHelper {
 
-    constructor(){
+    constructor() {
         throw new Error('Esta classe não pode ser instanciada!!');
     }
 
     static dataParaTexto(data) {
-        return data.getDate()
-            + '/' + (data.getMonth() + 1)
-            + '/' + data.getFullYear();
+        return `${data.getDate()}/${data.getMonth() + 1}/${data.getFullYear()}`;
     }
 
-    static textoParaData(texto){
+    static textoParaData(texto) {
+
+
+        //\d{4} - busca numerod(d) com 4 digitos
+        //\d{4} - busca numerod(d) com 2 digitos
+
+        const expressao = /\d{4}-\d{2}-\d{2}/;
+
+        if(!expressao.test(texto)){
+            throw new Error('Deve estar no formato yyyy-MM-dd');
+        }
+
         return new Date(...texto.split('-').map((item, indice) => item - indice % 2));
     }
 }
