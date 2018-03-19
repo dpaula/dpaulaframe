7b6c65cfec28b1d53a104d8008a2e8dc5b2acb3f
dpaula <fernando.dpaula@gmail.com>
2018-03-19 20:23:41 -0300

    trocada funçao normal por arrow function, pois seu this não muda em seu escopo

--- client/js/app/models/ListaNegociacoes.js
+++ client/js/app/models/ListaNegociacoes.js
@@ -1,27 +1,24 @@
 class ListaNegociacoes{
 
-    constructor(contexto, acao){
+    constructor(acao){
         this._negociacoes = [];
         this._acao = acao;
-        this._contexto = contexto;
     }
 
     adiciona(negociacao){
         this._negociacoes.push(negociacao);
-        //mudando o contexto da função atravez do metodo apply da classe Reflect
-        //parametros: a função a qual mudar, o contexto, e seus parametros
-        Reflect.apply(this._acao, this._contexto, [this]);
+        this._acao(this);
     }
-    
+
     get negociacoes(){
         //retornando uma nova referencia de negociações
         //programação defenciva para proteger as negociações    
         return [].concat(this._negociacoes);
     }
-    
+
     esvazia(){
         this._negociacoes = [];
-        Reflect.apply(this._acao, this._contexto, [this]);
+        this._acao(this);
     }
 
 }
