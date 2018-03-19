ffc8b697a3be1cd9c1c902b26654e1c73507c897
dpaula <fernando.dpaula@gmail.com>
2018-03-19 20:18:25 -0300

    usando classe de reflexao Reflect.apply para alterar o contexto da funçao

--- client/js/app/models/ListaNegociacoes.js
+++ client/js/app/models/ListaNegociacoes.js
@@ -1,21 +1,27 @@
 class ListaNegociacoes{
 
-    constructor(){
+    constructor(contexto, acao){
         this._negociacoes = [];
+        this._acao = acao;
+        this._contexto = contexto;
     }
 
     adiciona(negociacao){
         this._negociacoes.push(negociacao);
+        //mudando o contexto da função atravez do metodo apply da classe Reflect
+        //parametros: a função a qual mudar, o contexto, e seus parametros
+        Reflect.apply(this._acao, this._contexto, [this]);
     }
-
+    
     get negociacoes(){
         //retornando uma nova referencia de negociações
         //programação defenciva para proteger as negociações    
         return [].concat(this._negociacoes);
     }
-
+    
     esvazia(){
         this._negociacoes = [];
+        Reflect.apply(this._acao, this._contexto, [this]);
     }
 
 }
