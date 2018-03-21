3b39b12a4848c0fb3274c9c54638f6e4f6b2bc99
dpaula <fernando.dpaula@gmail.com>
2018-03-20 23:09:36 -0300

    criado metodo para importar nnegociacoes

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -1,54 +1,72 @@
-class NegociacaoController{
+class NegociacaoController {
 
-    constructor(){
+    constructor() {
+
         let $ = document.querySelector.bind(document);
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-        
+
         this._listaNegociacoes = new Bind(
-            new ListaNegociacoes(), 
-            new NegociacoesView($('#negociacoesView')), 
+            new ListaNegociacoes(),
+            new NegociacoesView($('#negociacoesView')),
             'adiciona', 'esvazia');
-       
+
+
         this._mensagem = new Bind(
             new Mensagem(), new MensagemView($('#mensagemView')),
-            'texto');       
-    }
-    
-    adiciona(event){
-        event.preventDefault();
-        
-        this._listaNegociacoes.adiciona(this._criaNegociacao());
-        this._mensagem.texto = 'Negociação adicionada com sucesso!!';
-        
-        this._limpaFormulario();
+            'texto');
     }
 
-    apaga(){
+    adiciona(event) {
+        event.preventDefault();
+
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._mensagem.texto = 'Negociação adicionada com sucesso!!';
+
+        this._limpaFormulario();
+
+    }
+
+    apaga() {
         this._listaNegociacoes.esvazia();
         this._mensagem.texto = 'Negociaçoes apagadas com sucesso !';
     }
-    
-    _criaNegociacao(){
+
+    _criaNegociacao() {
         let data = DataHelper.textoParaData(this._inputData.value);
-    
+
         return new Negociacao(
             data,
             this._inputQuantidade.value,
             this._inputValor.value
         );
-        
+
     }
 
-    _limpaFormulario(){
+    _limpaFormulario() {
         this._inputData.value = '';
         this._inputQuantidade.value = 0;
         this._inputValor.value = 0.0;
         this._inputData.focus();
     }
 
-    carregaIniciais(){
+    importaNegociacoes() {
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', 'negociacoes/semana');
+        xhr.onreadystatechange = () => {
+            if(xhr.readyState == 4) {
+                if(xhr.status == 200) {
+                    console.log('Obtendo as negociações do servidor.')
+                } else {
+                    console.log('Não foi possível obter as negociações do servidor.')
+                }
+            }
+        };
+        xhr.send();
+    }
+
+    carregaIniciais() {
         let $ = document.querySelector.bind(document);
         $('#data').value = '2015-01-01';
         $('#quantidade').value = '10';
